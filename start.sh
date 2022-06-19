616ec38db9a479041cebe1e001a9ea5c6cb8b73d
Przemysław Czerkas <pczerkas@gmail.com>
2022-06-19 21:32:15 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,54 +6,54 @@ CHOWN=$(/usr/bin/which chown)
 SQUID=$(/usr/bin/which squid)
 
 prepare_folders() {
-	echo "Preparing folders..."
-	mkdir -p /etc/squid-cert/
-	mkdir -p /var/cache/squid/
-	mkdir -p /var/log/squid/
-	"$CHOWN" -R squid:squid /etc/squid-cert/
-	"$CHOWN" -R squid:squid /var/cache/squid/
-	"$CHOWN" -R squid:squid /var/log/squid/
+    echo "Preparing folders..."
+    mkdir -p /etc/squid-cert/
+    mkdir -p /var/cache/squid/
+    mkdir -p /var/log/squid/
+    "$CHOWN" -R squid:squid /etc/squid-cert/
+    "$CHOWN" -R squid:squid /var/cache/squid/
+    "$CHOWN" -R squid:squid /var/log/squid/
 }
 
 initialize_cache() {
-	echo "Creating cache folder..."
-	"$SQUID" -z
+    echo "Creating cache folder..."
+    "$SQUID" -z
 
-	sleep 5
+    sleep 5
 }
 
 create_cert() {
-	if [ ! -f /etc/squid-cert/private.pem ]; then
-		echo "Creating certificate..."
-		openssl req -new -newkey rsa:2048 -sha256 -days 3650 -nodes -x509 \
-			-extensions v3_ca -keyout /etc/squid-cert/private.pem \
-			-out /etc/squid-cert/private.pem \
-			-subj "/CN=$CN/O=$O/OU=$OU/C=$C" -utf8 -nameopt multiline,utf8
+    if [ ! -f /etc/squid-cert/private.pem ]; then
+        echo "Creating certificate..."
+        openssl req -new -newkey rsa:2048 -sha256 -days 3650 -nodes -x509 \
+            -extensions v3_ca -keyout /etc/squid-cert/private.pem \
+            -out /etc/squid-cert/private.pem \
+            -subj "/CN=$CN/O=$O/OU=$OU/C=$C" -utf8 -nameopt multiline,utf8
 
-		openssl x509 -in /etc/squid-cert/private.pem \
-			-outform DER -out /etc/squid-cert/CA.der
+        openssl x509 -in /etc/squid-cert/private.pem \
+            -outform DER -out /etc/squid-cert/CA.der
 
-		openssl x509 -inform DER -in /etc/squid-cert/CA.der \
-			-out /etc/squid-cert/CA.pem
-	else
-		echo "Certificate found..."
-	fi
+        openssl x509 -inform DER -in /etc/squid-cert/CA.der \
+            -out /etc/squid-cert/CA.pem
+    else
+        echo "Certificate found..."
+    fi
 }
 
 clear_certs_db() {
-	echo "Clearing generated certificate db..."
-	rm -rfv /var/lib/ssl_db/
-	/usr/lib/squid/security_file_certgen -c -s /var/lib/ssl_db -M 20MB
-	"$CHOWN" -R squid.squid /var/lib/ssl_db
+    echo "Clearing generated certificate db..."
+    rm -rfv /var/cache/squid/ssl_db/
+    /usr/lib/squid/security_file_certgen -c -s /var/cache/squid/ssl_db -M 20MB
+    "$CHOWN" -R squid.squid /var/cache/squid/ssl_db
 }
 
 run() {
-	echo "Starting squid..."
-	prepare_folders
-	create_cert
-	clear_certs_db
-	initialize_cache
-	exec "$SQUID" -NYCd 1 -f /etc/squid/squid.conf
+    echo "Starting squid..."
+    prepare_folders
+    create_cert
+    clear_certs_db
+    initialize_cache
+    exec "$SQUID" -NYCd 1 -f /etc/squid/squid.conf
 }
 
 run
